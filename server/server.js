5292b508c0145dbe460d21207c75449bfe08f0e6
AsthaMakwana <102164078+AsthaMakwana@users.noreply.github.com>
2024-08-28 12:49:17 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,10 +15,11 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
     {
-        origin: 'https://musify-frontend-64ct.onrender.com',
-        credentials: true,
+        origin: [ 'https://musify-frontend-64ct.onrender.com', 'http://localhost:5173'],
+        credentials: true
     }
 ));
+
 app.use(express.json());
 
 // Routes
